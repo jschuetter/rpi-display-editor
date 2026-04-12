04881b41f64b97ae7b501fd250d366dcf0121b3b
jschuetter <100084569+jschuetter@users.noreply.github.com>
2026-04-12 14:41:56 -0400

    Layers panel, misc cleanup

--- src/Editor.py
+++ src/Editor.py
@@ -6,9 +6,10 @@ Holds source code for editor GUI
 '''
 
 import sys
-from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtCore import QPoint, Slot
-from PySide6.QtWidgets import QApplication, QWidget, QLayoutItem, QGridLayout, QHBoxLayout, QVBoxLayout, QLabel, QLineEdit
+from PySide6 import QtCore
+from PySide6.QtCore import Qt, QPoint, Slot
+from PySide6.QtWidgets import QApplication, QWidget, QLayoutItem, QGridLayout, QHBoxLayout, QVBoxLayout, QLabel, QLineEdit, QPushButton
+from PySide6.QtGui import QPainter, QPen, QBrush, QColor, QFont
 
 from src.Matrix import MatrixWidget, MatrixEmulatorWidget
 from src.Draggable import TextWidget, ImgWidget
@@ -17,9 +18,6 @@ from src.ScrollableMenu import ScrollableMenu
 class Editor(QApplication): 
     def __init__(self):
         super().__init__([])
-        # Store selected widget
-        self.selected = None 
-
         # Create application layout
         self.container = QWidget()
         self.applayout = QGridLayout(self.container)
@@ -35,14 +33,15 @@ class Editor(QApplication):
         self.matrix.add_widget(text)
 
         # Add editor menus
-        self.layers_menu = ScrollableMenu(QVBoxLayout, 200)
+        ## Layers menu
+        self.layers_menu = ScrollableMenu(QVBoxLayout, 400)
 
-        # Properties Menu
+        ## Properties Menu
         self.properties_menu = ScrollableMenu(QVBoxLayout)
 
         # Widget name
         self.prop_name = QLineEdit()
-        self.prop_name.textChanged.connect(self.push_props)
+        self.prop_name.editingFinished.connect(self.push_props)
         self.properties_menu.addWidget(QLabel("Name: "))
         self.properties_menu.addWidget(self.prop_name)
 
@@ -55,12 +54,13 @@ class Editor(QApplication):
         self.prop_pos_y = QLineEdit()
         self.prop_pos_x.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
         self.prop_pos_y.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
-        self.prop_pos_x.textChanged.connect(self.push_props)
-        self.prop_pos_y.textChanged.connect(self.push_props)
+        self.prop_pos_x.editingFinished.connect(self.push_props)
+        self.prop_pos_y.editingFinished.connect(self.push_props)
         self.propmenu_pos.layout().addWidget(self.prop_pos_x)
         self.propmenu_pos.layout().addWidget(self.prop_pos_y)
         self.properties_menu.addWidget(self.propmenu_pos)
 
+        ## Widget add menu
         self.add_menu = ScrollableMenu(QHBoxLayout, None, 200)
 
         self.applayout.addWidget(self.layers_menu, 0, 0)
@@ -71,6 +71,7 @@ class Editor(QApplication):
         # DEBUG TESTING
         self.matrix.selected_idx = -1
         self.matrix.subscribe_selected_updates(self.pull_props)
+        self.matrix.subscribe_selected_updates(self.update_layers)
 
         self.container.showMaximized()
 
@@ -102,3 +103,68 @@ class Editor(QApplication):
         sw_pos = sw.mat_bb.topLeft()
         self.prop_pos_x.setText(str(sw_pos.x()))
         self.prop_pos_y.setText(str(sw_pos.y()))
+
+    def update_layers(self): 
+        '''
+        Update layers menu with current widget list
+        '''
+        self.layers_menu.empty()
+        for w_idx in range(1, len(self.matrix.widgets)): # Start loop at 1 to ignore background widget(?)
+            widget = self.matrix.widgets[w_idx]
+            list_item = LayersItem(widget.name, self.matrix, w_idx)
+            
+            if w_idx == self.matrix.selected_idx: 
+                pass
+
+            self.layers_menu.addWidget(list_item)
+
+class LayersItem(QWidget):
+    FONT = QFont("Arial", 11)
+    BTN_WIDTH = 30
+    def __init__(self, name, matrix, idx, parent=None):
+        super().__init__(parent)
+        self.mat = matrix
+        self.w_idx = idx
+        self.setLayout(QHBoxLayout())
+        self.label = QLabel(name)
+        self.label.setFont(self.FONT)
+        self.setMinimumHeight(self.FONT.pointSize())
+
+        self.up_btn = QPushButton('↑')
+        self.up_btn.clicked.connect(lambda: self.mat.move_widget_up(self.w_idx))
+        self.up_btn.setMinimumWidth(self.BTN_WIDTH)
+
+        self.down_btn = QPushButton('↓')
+        self.up_btn.clicked.connect(lambda: self.mat.move_widget_down(self.w_idx))
+        self.down_btn.setMinimumWidth(self.BTN_WIDTH)
+
+        self.layout().addWidget(self.label)
+        self.layout().addStretch(20)
+        self.layout().addWidget(self.up_btn)
+        self.layout().setStretch(2, 1)
+        self.layout().addWidget(self.down_btn)
+        self.layout().setStretch(3, 1)
+
+    def mousePressEvent(self, e):
+        '''
+        Update matrix selected_idx when clicked
+        '''
+        if e.button() == Qt.LeftButton:
+            self.mat.set_selected(self.w_idx)
+
+    def paintEvent(self, event):
+        '''
+        Override default paintEvent() method
+        Custom paint event to show box around widget when selected
+        '''
+        # If not selected, skip draw
+        if not self.w_idx == self.mat.selected_idx: 
+            return
+
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        pen = QPen(QColor(255,145,0))
+        pen.setWidth(3)
+        painter.setPen(pen)
+        painter.setBrush(QBrush(QColor(0, 0, 0, 0)))  # Transparent fill
+        painter.drawRect(0, 0, self.width(), self.height())
